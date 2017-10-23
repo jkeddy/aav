162d9725f572991a1bcbfc5b009cad443d85b837
Unknown <jeff.keddy@gmail.com>
2017-10-23 11:29:33 -0500

    nav updates.
    mobile nav introduced. sub nav toggle system built

--- src/_js/main-nav.js
+++ src/_js/main-nav.js
@@ -1,13 +1,12 @@
 function toggleNav(){
     var mainNav = document.getElementById('main-nav');
-    var check = mainNav.classList.contains('active');
+    var navOpen = mainNav.classList.contains('active');
     var scrim = 'nav-scrim';
-    if(check){
+    if(navOpen){
         destroyScrim(scrim);
         document.body.classList.remove('nav-active');
         mainNav.classList.remove('active');
-    }
-    else{
+    }else{
         createScrim(scrim);
         document.body.classList.add('nav-active');
         mainNav.classList.add('active');
@@ -15,7 +14,31 @@ function toggleNav(){
         document.getElementById(scrim).addEventListener('mouseup',toggleNav);
     }
 }
+function toggleSubNav(){
+    var t = this;
+    var icon = t.firstElementChild;
+    var subNav = t.nextElementSibling;
+    var subNavOpen = subNav.classList.contains('active');
+    var parent = t.parentElement;
+    if(subNavOpen){
+        parent.classList.remove('active');
+        t.classList.remove('active');
+        icon.classList.remove('fa-caret-down');
+        icon.classList.add('fa-caret-right');
+        subNav.classList.remove('active');
+    }else{
+        parent.classList.add('active');
+        t.classList.add('active');
+        icon.classList.add('fa-caret-down');
+        icon.classList.remove('fa-caret-right');
+        subNav.classList.add('active');
+    }
+}
 var navToggle=document.getElementsByClassName('main-nav__toggle');
 for (var i = 0; i < navToggle.length; i++) {
     navToggle[i].addEventListener('mouseup',toggleNav);
+}
+var subNavToggle=document.getElementsByClassName('main-nav__sub-nav-toggle');
+for (var j = 0; j < subNavToggle.length; j++) {
+    subNavToggle[j].addEventListener('click',toggleSubNav);
 }
