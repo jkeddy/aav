b609b6a1135a3b19e2fb13ae829055e8966386ed
Jeff K <jeff.keddy@gmail.com>
2017-12-07 15:28:01 -0600

    sticky fallback updates.

--- src/_js/sticky-fallback.js
+++ src/_js/sticky-fallback.js
@@ -3,36 +3,50 @@ Author: Aurelio De Rosa
 Edited by: Jeff Keddy
 Date: 12/06/2017
 Description: A falback method for browsers that do not support CSS sticky. See article: http://www.sitepoint.com/css-position-sticky-introduction-polyfills/
-TODO:   Add throttling to scroll event.
-        Need to figure out a way to turn on place holder based on screen size and relevant parent node.
+TODO:   Add throttling to scroll event. Will require lodash so not a priority.
+        Must be a better way to call this on all fallback events.
 */
 
 function posStickySupported() {
-    var e = document.createElement('div');
+    let e = document.createElement('div');
     e.style.cssText = 'position:sticky';
-    if (e.style.position.match('sticky')) return false;
+    if (e.style.position.match('sticky')) return true;
     return false;
 }
-
-if (!posStickySupported()) {
-    var elemStuck = document.querySelectorAll('.js-sticky-fallback');
-    elemStuck.forEach(function (e) {
-        var elemPos = e.getBoundingClientRect();
-        var placeholder = document.createElement('div');
-        placeholder.classList.add('_sticky-ph');
-        placeholder.style.width = elemPos.width + 'px';
-        placeholder.style.height = elemPos.height + 'px';
-        var isAdded = false;
-        window.addEventListener('scroll', function () {
-            if (window.pageYOffset >= elemPos.top && !isAdded) {
-                e.classList.add('_sticky');
-                e.parentNode.insertBefore(placeholder, e);
-                isAdded = true;
-            } else if (window.pageYOffset < elemPos.top && isAdded) {
-                e.classList.remove('_sticky');
-                e.parentNode.removeChild(placeholder);
-                isAdded = false;
-            }
-        });
+function posStickyFallback(elem) {
+    let elemStuck = document.querySelector(elem);
+    let elemPos = elemStuck.getBoundingClientRect();
+    let placeholder = document.createElement('div');
+    placeholder.style.width = elemPos.width + 'px';
+    placeholder.style.height = elemPos.height + 'px';
+    let isAdded = false;
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset >= elemPos.top && !isAdded) {
+            elemStuck.classList.add('_sticky');
+            elemStuck.parentNode.insertBefore(placeholder, elemStuck);
+            isAdded = true;
+        } else if (window.pageYOffset < elemPos.top && isAdded) {
+            elemStuck.classList.remove('_sticky');
+            elemStuck.parentNode.removeChild(placeholder);
+            isAdded = false;
+        }
     });
+    return true;
+}
+if (!posStickySupported()) {
+    // Side content >=layout
+    if(window.matchMedia('(min-width: 850px)').matches) {
+        posStickyFallback('.c-side-content__inner.js-sticky-fallback');
+        console.log('side')
+    }
+    // Nav <=tablet","height>=600px"
+    if(window.matchMedia('(max-width: 768px) and (min-width: 550px) and (min-height: 600px)').matches){
+        posStickyFallback('.c-main-nav__inner.js-sticky-fallback');
+        console.log('nav')
+    }
+    // Header <=screen-sm
+    if(window.matchMedia('(max-width: 550px)').matches){
+        posStickyFallback('.c-header.js-sticky-fallback');
+        console.log('header')
+    }
 }
