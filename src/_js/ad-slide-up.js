43b4646ac9ec360d71118bf9d2a97217c6df321b
Jeff K <jeff.keddy@gmail.com>
2017-12-20 12:27:19 -0600

    adding a cookie to the ad slide up.

--- src/_js/ad-slide-up.js
+++ src/_js/ad-slide-up.js
@@ -2,33 +2,30 @@
 Author: Jeff Keddy
 Date: 12/15/2017
 Description: Slides AAV slide up ad into view when scrolled 5% down screen
-TODO:   Add closing cookie
 */
-
-/*
-let path = window.location.pathname.split('/')
-let l = path.length
-let fileName = 'name=' + path[l - 1]
-document.cookie = fileName
-console.log(document.cookie)
-*/
-const adBottom = document.getElementById('sLd_exp')
-console.log(adBottom)
-if (adBottom) {
+window.onload = function () {
+  const adBottom = document.getElementById('sLd_exp')
+  let path = window.location.pathname.split('/')
+  let l = path.length
+  let fileName = path[l - 2]
+  let slideCookie = getCookie(fileName)
+  if (!slideCookie && adBottom) {
     let e = document.createElement('button')
     e.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
     e.classList.add('aavd__close')
     adBottom.appendChild(e)
     e.addEventListener('click', function () {
-        adBottom.parentNode.removeChild(adBottom)
+      createCookie(fileName, 'allaboutvision.com', 30)
+      adBottom.parentNode.removeChild(adBottom)
     })
     window.addEventListener('scroll', function () {
-        let body = document.body.offsetHeight
-        let offset = Math.round(body * 0.05) // Open after 5% scroll
-        if (window.pageYOffset >= offset) {
-            adBottom.classList.add('is-open')
-        } else if (window.pageYOffset < offset) {
-            adBottom.classList.remove('is-open')
-        }
+      let body = document.body.offsetHeight
+      let offset = Math.round(body * 0.05) // Open after 5% scroll
+      if (window.pageYOffset >= offset) {
+        adBottom.classList.add('is-open')
+      } else if (window.pageYOffset < offset) {
+        adBottom.classList.remove('is-open')
+      }
     })
+  }
 }
