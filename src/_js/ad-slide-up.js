ad3e5320073db1cd57c0904ff265e4d7895bffe9
Jeff K <jeff.keddy@gmail.com>
2017-12-21 15:56:02 -0600

    adding media query to match when this add no longer loads

--- src/_js/ad-slide-up.js
+++ src/_js/ad-slide-up.js
@@ -3,29 +3,35 @@ Author: Jeff Keddy
 Date: 12/15/2017
 Description: Slides AAV slide up ad into view when scrolled 5% down screen
 */
-window.onload = function () {
+// >=screen-sm
+if (window.matchMedia('(min-width: 550px)').matches) {
   const adBottom = document.getElementById('sLd_exp')
-  let path = window.location.pathname.split('/')
-  let l = path.length
-  let fileName = path[l - 2]
-  let slideCookie = getCookie(fileName)
-  if (!slideCookie && adBottom) {
-    let e = document.createElement('button')
-    e.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
-    e.classList.add('aavd__close')
-    adBottom.appendChild(e)
-    e.addEventListener('click', function () {
-      createCookie(fileName, 'allaboutvision.com', 30)
-      adBottom.parentNode.removeChild(adBottom)
-    })
-    window.addEventListener('scroll', function () {
-      let body = document.body.offsetHeight
-      let offset = Math.round(body * 0.05) // Open after 5% scroll
-      if (window.pageYOffset >= offset) {
-        adBottom.classList.add('is-open')
-      } else if (window.pageYOffset < offset) {
-        adBottom.classList.remove('is-open')
-      }
-    })
+
+  window.onload = function () {
+    let path = window.location.pathname.split('/')
+    let l = path.length
+    let fileName = path[l - 2]
+    let slideCookie = getCookie(fileName)
+    if (!slideCookie && adBottom) {
+      document.body.classList.add('slide-up-is-active')
+      let e = document.createElement('button')
+      e.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
+      e.classList.add('aavd__close')
+      adBottom.appendChild(e)
+      e.addEventListener('click', function () {
+        createCookie(fileName, 'allaboutvision.com', 1)
+        document.body.classList.remove('slide-up-is-active')
+        adBottom.parentNode.removeChild(adBottom)
+      })
+      window.addEventListener('scroll', function () {
+        let body = document.body.offsetHeight
+        let offset = Math.round(body * 0.05) // Open after 5% scroll
+        if (window.pageYOffset >= offset) {
+          adBottom.classList.add('is-open')
+        } else if (window.pageYOffset < offset) {
+          adBottom.classList.remove('is-open')
+        }
+      })
+    }
   }
 }
