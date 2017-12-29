75a418268a9cb4d1963aa20c2d2b3f06b4cf9070
Jeff K <jeff.keddy@gmail.com>
2017-12-29 15:38:20 -0600

    swapping to a better cookie system

--- src/_js/cookies.js
+++ src/_js/cookies.js
@@ -1,23 +1,20 @@
-function createCookie (name, value, expires, path, domain) {
-  let cookie = name + '=' + escape(value) + ';'
-  if (expires) {
-    // If it's a date
-    if (expires instanceof Date) {
-      // If it isn't a valid date
-      if (isNaN(expires.getTime())) {
-        expires = new Date()
-      }
-    } else {
-      expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24)
-    }
-    cookie += 'expires=' + expires.toGMTString() + ';'
-  }
-  if (path) { cookie += 'path=' + path + ';' }
-  if (domain) { cookie += 'domain=' + domain + ';' }
-  document.cookie = cookie
+function createCookie (name, value, days) {
+  let expires
+  if (days) {
+    let date = new Date()
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+    expires = '; expires=' + date.toGMTString()
+  } else expires = ''
+  document.cookie = name + '=' + value + expires + '; path=/'
+  console.log(document.cookie)
 }
 function getCookie (name) {
-  let regexp = new RegExp('(?:^' + name + '|;\s*' + name + ')=(.*?)(?:;|$)', 'g')
-  let result = regexp.exec(document.cookie)
-  return (result === null) ? null : result[1]
+  let nameEQ = name + '='
+  let ca = document.cookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+  }
+  return null
 }
